0115cc819fb1194c1205aac63e99df34d4dbc49a
Alexander Young <alex1001@live.ca>
2021-12-30 21:39:05 -0700

    Update to v4

--- SCPSwap/SCPSwap.cs
+++ SCPSwap/SCPSwap.cs
@@ -8,26 +8,37 @@ namespace SCPSwap
 		public EventHandlers Handler { get; private set; }
 		public override string Name => nameof(ScpSwap);
 		public override string Author => "Cyanox";
+		private bool state = false;
 
 		public ScpSwap() { }
 
 		public override void OnEnabled()
 		{
+			if (state) return;
+
 			Instance = this;
 			Handler = new EventHandlers(this);
 			Exiled.Events.Handlers.Server.WaitingForPlayers += Handler.OnWaitingForPlayers;
 			Exiled.Events.Handlers.Server.RoundStarted += Handler.OnRoundStart;
 			Exiled.Events.Handlers.Server.RoundEnded += Handler.OnRoundEnd;
 			Exiled.Events.Handlers.Server.RestartingRound += Handler.OnRoundRestart;
+
+			state = true;
+			base.OnEnabled();
 		}
 
 		public override void OnDisabled()
 		{
+			if (!state) return;
+
 			Exiled.Events.Handlers.Server.WaitingForPlayers -= Handler.OnWaitingForPlayers;
 			Exiled.Events.Handlers.Server.RoundStarted -= Handler.OnRoundStart;
 			Exiled.Events.Handlers.Server.RoundEnded -= Handler.OnRoundEnd;
 			Exiled.Events.Handlers.Server.RestartingRound -= Handler.OnRoundRestart;
 			Handler = null;
+
+			state = false;
+			base.OnEnabled();
 		}
 
 		public override void OnReloaded() { }
